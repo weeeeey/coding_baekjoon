2abf11205e406cfe21b0ce08b923c9a7b5c0944f
weeeeey <96678855+weeeeey@users.noreply.github.com>
2022-01-14 18:22:55 +0900

    Update 14888_연산자_끼워넣기.py

--- 14888_연산자_끼워넣기.py
+++ 14888_연산자_끼워넣기.py
@@ -8,6 +8,8 @@ n개의 수들이 주어졌을때 연산자를 끼워 넣어 최대값과 최소
 # arr이라는 리스트를 우선순위 q에 넣었다고 가정했을떄
 # q에 넣은후에 arr을 조작하면 q에 들어가있는 arr에 해당하는 리스트도 같이 조작되어버림
 # 이걸 몰라서 겁나 해맴
+
+#[BFS로 품]
 from collections import deque
 num = int(input())
 arr = list(map(int,input().split()))
@@ -41,3 +43,35 @@ while(q):
           q.append(((ans*(-1)//arr[cnt])*(-1),[a,b,c,d+1],cnt+1))
 print(max(result))
 print(min(result))
+
+#[DFS]로 품
+num = int(input())
+data = list(map(int,input().split()))
+p, s, m, d = map(int,input().split())
+max_v = -1e9
+min_v = 1e9
+def DFS(n,now):
+  global p,s,m,d,max_v,min_v
+  if n == num:
+    max_v = max(max_v,now)
+    min_v = min(min_v,now)                   
+  else:  
+    if p>0:
+      p-=1
+      DFS(n+1,now+data[n])
+      p+=1
+    if s>0:
+      s-=1
+      DFS(n+1,now-data[n])
+      s+=1
+    if m>0:
+      m-=1
+      DFS(n+1,now*data[n])
+      m+=1
+    if d>0:
+      d-=1
+      DFS(n+1,(-1)*(now*(-1)//data[n]))
+      d+=1
+DFS(1,data[0])
+print(max_v)
+print(min_v)
